cb470cc3808395ed38cbc4af178e0b1847b30b21
Tomas Garcia <tomas.garcia@zolar.de>
2024-09-25 15:47:49 +0200

    Creating Routes and Upgrading Router 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FeedbackList from "./components/FeedbackList";
 import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -21,14 +23,29 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,17 @@
+import Card from "../components/shared/Card";
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About This Project</h1>
+        <p>This is a React to leave feedback for a product or service</p>
+        <p>Version: 1.0.0</p>
+
+        <p></p>
+      </div>
+    </Card>
+  );
+}
+
+export default AboutPage;
